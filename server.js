2f715dfd2c7336fc8ca9e74cd4b8b85cc130b2c7
gdaskevics <g.daskevics@students.htl-leonding.ac.at>
2025-03-18 12:25:58 +0100

    Fixed port

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
 app.get("/", (req, res) => {
     res.send("YouTube to Spotify Playlist Converter is running!");
